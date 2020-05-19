38d8e276a3708d6b59e50344e9b4a15901f8665f
EC2 Default User <ec2-user@ip-172-31-34-55.eu-central-1.compute.internal>
2020-05-19 15:16:05 +0000

    added git submodule update

--- .platform/hooks/prebuild/tarsalgo-prebuild.sh
+++ .platform/hooks/prebuild/tarsalgo-prebuild.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 echo tarsalgo-prebuild-hook-started
 sudo yum -y install postgresql-devel
+echo listing-staging-folder
+ls -la /var/app/staging
+runuser -l webapp -c "git submodule update --init --recursive"
 runuser -l webapp -c 'whoami && pwd && cd /var/app/staging/ && pwd && npm install'
 echo tarsalgo-prebuild-hook-finihsed
